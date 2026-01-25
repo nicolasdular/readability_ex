32a0511c293d699159144319927e2a6b34deba2e
Nicolas Dular <hello@nicolasdular.com>
2026-01-25 10:01:27 +0100

    Add checksum for v0.1.8

---
+++ checksum-Elixir.ReadabilityEx.exs
@@ -0,0 +1,8 @@
+%{
+  "libreadability_ex-v0.1.8-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a3a6f27abc066bf16596aae76688cde7fbab1e97f1b220982b74e47ceafab627",
+  "libreadability_ex-v0.1.8-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:4a67ffa0c56b2a722548a0b89ac9b7c8e048d5aeb76f36524f53c337a2279880",
+  "libreadability_ex-v0.1.8-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:b016c88e8568c9ecb036cd0b86501d1228831550bb2bdc6cd27b73efe4d391b0",
+  "libreadability_ex-v0.1.8-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:6679234e9de9360a68e8804fd2d6ed6ee91d54e4b006415663020b2a3e18c148",
+  "libreadability_ex-v0.1.8-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3dfb01f703a6f37d853d9b3a0f211368e4a1e4a603d37c489bd2560880d168a0",
+  "libreadability_ex-v0.1.8-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:d79c5789b29ad28e3463d6115eb65ab9bc827201d4440aa8ce23e4d623f92261",
+}
