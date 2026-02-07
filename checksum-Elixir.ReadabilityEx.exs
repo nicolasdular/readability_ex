96910e9033526802f3bc01a08e91aca3fdb7897a
Nicolas Dular <hello@nicolasdular.com>
2026-02-07 20:54:14 +0100

    Update precompiled NIF checksums

--- checksum-Elixir.ReadabilityEx.exs
+++ checksum-Elixir.ReadabilityEx.exs
@@ -1,8 +1,8 @@
 %{
-  "libreadability_ex-v0.1.8-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a3a6f27abc066bf16596aae76688cde7fbab1e97f1b220982b74e47ceafab627",
-  "libreadability_ex-v0.1.8-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:4a67ffa0c56b2a722548a0b89ac9b7c8e048d5aeb76f36524f53c337a2279880",
-  "libreadability_ex-v0.1.8-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:b016c88e8568c9ecb036cd0b86501d1228831550bb2bdc6cd27b73efe4d391b0",
-  "libreadability_ex-v0.1.8-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:6679234e9de9360a68e8804fd2d6ed6ee91d54e4b006415663020b2a3e18c148",
-  "libreadability_ex-v0.1.8-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3dfb01f703a6f37d853d9b3a0f211368e4a1e4a603d37c489bd2560880d168a0",
-  "libreadability_ex-v0.1.8-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:d79c5789b29ad28e3463d6115eb65ab9bc827201d4440aa8ce23e4d623f92261",
+  "libreadability_ex-v0.1.11-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a3473698b470ece049d9b55764823412d8f53a5eb19671b8450b9d54991926d5",
+  "libreadability_ex-v0.1.11-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9dcb30000a29f92b41fea3d40c18ffeddb9fbcbc3c5aec050fab81b5d4c3da14",
+  "libreadability_ex-v0.1.11-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:7c985ca5e687fb7d4e20f778918ab3127ab436626bf9a09535572d7bfa7f3a8e",
+  "libreadability_ex-v0.1.11-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:9ac49c25963c98be41188f8a8d336338b9a08e7a0dfeb6872123966be8b440d0",
+  "libreadability_ex-v0.1.11-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:fade7bb14046ff2f194d0d241e2c0a055c01bc396dff2bbf2b74d4fcda323587",
+  "libreadability_ex-v0.1.11-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:4236f2a2394abcc1acddc3e0f7e66a1e6d17dce710ce641cea30d82025bf72c5",
 }
