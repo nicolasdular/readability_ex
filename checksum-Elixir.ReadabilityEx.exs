e86e9a59265d3dfd8aaae1738333e4f68c3646a8
Nicolas Dular <hello@nicolasdular.com>
2026-02-07 21:07:48 +0100

    Refresh precompiled NIF checksums

--- checksum-Elixir.ReadabilityEx.exs
+++ checksum-Elixir.ReadabilityEx.exs
@@ -1,8 +1,8 @@
 %{
-  "libreadability_ex-v0.1.11-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:7ca01ffb2b932fedad1ffe1ffb9981427afa37d8df1757f9ac0bf45c2372040d",
-  "libreadability_ex-v0.1.11-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:432490b896dc2be3e3f65ee6aeb8197f3324b0e6d9996acc73ccd5a2b212a36a",
-  "libreadability_ex-v0.1.11-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:a49bff39c5a002de6d8d71894c639e33ecbb8244a9812b91dfd51c5a35556a3c",
-  "libreadability_ex-v0.1.11-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:2236c14e3f70bae4737137061259659aa99c5dae0783202a3aa04fbbedca389c",
-  "libreadability_ex-v0.1.11-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4a732b87ece9f4860a93eb16530834f609e7528a16cdc6235ed3bff32a4972fd",
-  "libreadability_ex-v0.1.11-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:af3cbf2621ed71e15c5c5bc01f19f2c3a67310080589be9a8ac9192ecca12893",
+  "libreadability_ex-v0.1.11-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:6fbe8a84c1e8453dac5e1e74793a81b4e5e8053c943a48c57c53b555b67840b4",
+  "libreadability_ex-v0.1.11-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:bf4f2b37f158434076a1946a92ce444fb806fcdd077b40ae43ca047159162b59",
+  "libreadability_ex-v0.1.11-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:5f810fa308d48dab8fdbf3f9bef2bc0a7fa28197c48c7b448fbeec0e7283dfc5",
+  "libreadability_ex-v0.1.11-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b8973059d9f405d42cb0eae36c177bb4e9e80d03eb2ff1161323173f524b66ce",
+  "libreadability_ex-v0.1.11-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:fb1c21c20e60a09ee9d19e156a8ced358d0b776ff183c2b597aad286439216f4",
+  "libreadability_ex-v0.1.11-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:944c654d3591535bc2a1e2dcc162e056018d274bbd8b08c3f222a9c452c821bd",
 }
